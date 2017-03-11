4020411f2070b0a7b4248f28d649dd6cef9a9e6d
Verdan Mahmood <verdan.mahmood@heaptech.com>
2017-03-11 23:54:13 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,207 +1,259 @@
-import React from 'react';
-import d3 from 'd3';
-import nv from 'nvd3';
-import {
-  pick,
-  without,
-  isPlainObject,
-  bindFunctions,
-  getValueFunction,
-  propsByPrefix,
-  isCallable
-} from './utils.js';
+'use strict';
 
-let SETTINGS = ['x', 'y', 'type', 'datum', 'configure'];
-let SIZE = ['width', 'height'];
-let MARGIN = 'margin';
-let LEGEND = 'legend';
-let TOOLTIP = 'tooltip';
-let CONTAINER_STYLE = 'containerStyle'
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
 
-const RENDER_START = 'renderStart';
-const ELEMENT_CLICK = 'elementClick';
-const RENDER_END = 'renderEnd';
-const READY = 'ready';
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-export default class NVD3Chart extends React.Component {
-  static propTypes: {
-    type: React.PropTypes.string.isRequired,
-    configure: React.PropTypes.func
-  };
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  constructor() {
-        super();
+var _react = require('react');
 
-        // bind "this" at constructor stage so that function is available to be removed from window resize event on unmount
-        this.resize = this.resize.bind(this);
-  }
+var _react2 = _interopRequireDefault(_react);
 
-  /**
-   * Instantiate a new chart setting
-   * a callback if exists
-   */
-  componentDidMount() {
-    nv.addGraph(this.renderChart.bind(this), (chart) => {
-      if(isCallable(this.props.ready)) this.props.ready(chart, READY);
-    });
-  }
+var _d = require('d3');
 
-  /**
+var _d2 = _interopRequireDefault(_d);
+
+var _nvd = require('nvd3');
+
+var _nvd2 = _interopRequireDefault(_nvd);
+
+var _utils = require('./utils.js');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var SETTINGS = ['x', 'y', 'type', 'datum', 'configure'];
+var SIZE = ['width', 'height'];
+var MARGIN = 'margin';
+var LEGEND = 'legend';
+var TOOLTIP = 'tooltip';
+var CONTAINER_STYLE = 'containerStyle';
+
+var RENDER_START = 'renderStart';
+var ELEMENT_CLICK = 'elementClick';
+var RENDER_END = 'renderEnd';
+var READY = 'ready';
+
+var NVD3Chart = function (_React$Component) {
+	_inherits(NVD3Chart, _React$Component);
+
+	function NVD3Chart() {
+		_classCallCheck(this, NVD3Chart);
+
+		// bind "this" at constructor stage so that function is available to be removed from window resize event on unmount
+		var _this = _possibleConstructorReturn(this, (NVD3Chart.__proto__ || Object.getPrototypeOf(NVD3Chart)).call(this));
+
+		_this.resize = _this.resize.bind(_this);
+		return _this;
+	}
+
+	/**
+  * Instantiate a new chart setting
+  * a callback if exists
+  */
+
+
+	_createClass(NVD3Chart, [{
+		key: 'componentDidMount',
+		value: function componentDidMount() {
+			var _this2 = this;
+
+			_nvd2.default.addGraph(this.renderChart.bind(this), function (chart) {
+				if ((0, _utils.isCallable)(_this2.props.ready)) _this2.props.ready(chart, READY);
+			});
+		}
+
+		/**
    * Update the chart after state is changed.
    */
-  componentDidUpdate() {
-    this.renderChart();
-  }
 
-  /**
+	}, {
+		key: 'componentDidUpdate',
+		value: function componentDidUpdate() {
+			this.renderChart();
+		}
+
+		/**
    * Remove listeners
    */
-  componentWillUnmount() {
-    if(this.resizeHandler)
-        clearTimeout(this.resizeHandler);
-        if (window.removeEventListener)
-                window.removeEventListener("resize", this.resize);
-        else
-                window.detachEvent("resize", this.resize);
-  }
 
-  /**
+	}, {
+		key: 'componentWillUnmount',
+		value: function componentWillUnmount() {
+			if (this.resizeHandler) clearTimeout(this.resizeHandler);
+			if (window.removeEventListener) window.removeEventListener("resize", this.resize);else window.detachEvent("resize", this.resize);
+		}
+
+		/**
    * Creates a chart model and render it
    */
-  renderChart() {
-      let dispatcher;
 
-      // We try to reuse the current chart instance. If not possible then lets instantiate again
-      this.chart = (this.chart && !this.rendering) ? this.chart : nv.models[this.props.type]();
+	}, {
+		key: 'renderChart',
+		value: function renderChart() {
+			var dispatcher = void 0;
 
-      if(isCallable(this.props.renderStart))
-        this.props.renderStart(this.chart, RENDER_START);
+			// We try to reuse the current chart instance. If not possible then lets instantiate again
+			this.chart = this.chart && !this.rendering ? this.chart : _nvd2.default.models[this.props.type]();
 
-      this.parsedProps = bindFunctions(this.props, this.props.context);
+			if ((0, _utils.isCallable)(this.props.renderStart)) this.props.renderStart(this.chart, RENDER_START);
 
-      this.chart.x && this.chart.x(getValueFunction(this.parsedProps.x, 'x'));
-      this.chart.y && this.chart.y(getValueFunction(this.parsedProps.y, 'y'));
-      this.props.margin && this.chart.margin(this.options(MARGIN, pick).margin || propsByPrefix('margin', this.props) || {});
+			this.parsedProps = (0, _utils.bindFunctions)(this.props, this.props.context);
 
-      // Configure componentes recursively
-      this.configureComponents(this.chart, this.options(SETTINGS.concat(CONTAINER_STYLE), without));
+			this.chart.x && this.chart.x((0, _utils.getValueFunction)(this.parsedProps.x, 'x'));
+			this.chart.y && this.chart.y((0, _utils.getValueFunction)(this.parsedProps.y, 'y'));
+			this.props.margin && this.chart.margin(this.options(MARGIN, _utils.pick).margin || (0, _utils.propsByPrefix)('margin', this.props) || {});
 
-      // hook for configuring the chart
-      !this.props.configure || this.props.configure(this.chart);
+			// Configure componentes recursively
+			this.configureComponents(this.chart, this.options(SETTINGS.concat(CONTAINER_STYLE), _utils.without));
 
-      // Render chart using d3
-      this.selection = d3.select(this.refs.svg)
-        .datum(this.props.datum)
-        .call(this.chart);
+			// hook for configuring the chart
+			!this.props.configure || this.props.configure(this.chart);
 
-      // Update the chart if the window size change.
-      // Save resizeHandle to remove the resize listener later.
-      if(!this.resizeHandler)
-                if (window.addEventListener)
-                        window.addEventListener("resize", this.resize, false);
-                else
-                        window.attachEvent("resize", this.resize, false);
+			// Render chart using d3
+			this.selection = _d2.select(this.refs.svg).datum(this.props.datum).call(this.chart);
 
-      // PieCharts and lineCharts are an special case. Their dispacher is the pie component inside the chart.
-      // There are some charts do not feature the renderEnd event
-      switch(this.props.type) {
-        case 'multiBarChart':
-          dispatcher = this.chart.multibar.dispatch;
-          break
-        case 'pieChart':
-          dispatcher = this.chart.pie.dispatch;
-          break
-        case 'lineChart':
-        case 'linePlusBarChart':
-          dispatcher = this.chart.lines.dispatch;
-          break
-        default:
-          dispatcher = this.chart.dispatch;
-      }
+			// Update the chart if the window size change.
+			// Save resizeHandle to remove the resize listener later.
+			if (!this.resizeHandler) if (window.addEventListener) window.addEventListener("resize", this.resize, false);else window.attachEvent("resize", this.resize, false);
 
-      dispatcher.renderEnd && dispatcher.on('renderEnd', this.renderEnd.bind(this));
-      dispatcher.elementClick && dispatcher.on('elementClick', this.elementClick.bind(this));
-      this.rendering = true;
+			// PieCharts and lineCharts are an special case. Their dispacher is the pie component inside the chart.
+			// There are some charts do not feature the renderEnd event
+			switch (this.props.type) {
+				case 'multiBarChart':
+					dispatcher = this.chart.multibar.dispatch;
+					break;
+				case 'pieChart':
+					dispatcher = this.chart.pie.dispatch;
+					break;
+				case 'lineChart':
+				case 'linePlusBarChart':
+					dispatcher = this.chart.lines.dispatch;
+					break;
+				default:
+					dispatcher = this.chart.dispatch;
+			}
 
-      return this.chart;
-  }
+			dispatcher.renderEnd && dispatcher.on('renderEnd', this.renderEnd.bind(this));
+			dispatcher.elementClick && dispatcher.on('elementClick', this.elementClick.bind(this));
+			this.rendering = true;
 
-  /**
+			return this.chart;
+		}
+
+		/**
    * Render end callback function
    * @param  {Event} e
    */
-  renderEnd(e) {
-    if(isCallable(this.props.renderEnd))
-      this.props.renderEnd(this.chart, RENDER_END);
-    // Once renders end then we set rendering to false to allow to reuse the chart instance.
-    this.rendering = false;
-  }
 
-  /**
+	}, {
+		key: 'renderEnd',
+		value: function renderEnd(e) {
+			if ((0, _utils.isCallable)(this.props.renderEnd)) this.props.renderEnd(this.chart, RENDER_END);
+			// Once renders end then we set rendering to false to allow to reuse the chart instance.
+			this.rendering = false;
+		}
+
+		/**
    * element click callback function
    * @param  {Event} e
    */
-  elementClick(e) {
-    if(isCallable(this.props.elementClick))
-      this.props.elementClick(e, 'elementClick');
-  }
 
-  /**
+	}, {
+		key: 'elementClick',
+		value: function elementClick(e) {
+			if ((0, _utils.isCallable)(this.props.elementClick)) this.props.elementClick(e, 'elementClick');
+		}
+
+		/**
    * Configure components recursively
    * @param {nvd3 chart} chart  A nvd3 chart instance
    * @param {object} options    A key value object
    */
-  configureComponents(chart, options) {
-    for(let optionName in options){
-      let optionValue = options[optionName];
-      if(chart) {
-        if(isPlainObject(optionValue)){
-          this.configureComponents(chart[optionName], optionValue);
-        } else if(typeof chart[optionName] === 'function'){
-          chart[optionName](optionValue);
-        }
-      }
-    }
-  }
 
-  /**
+	}, {
+		key: 'configureComponents',
+		value: function configureComponents(chart, options) {
+			for (var optionName in options) {
+				var optionValue = options[optionName];
+				if (chart) {
+					if ((0, _utils.isPlainObject)(optionValue)) {
+						this.configureComponents(chart[optionName], optionValue);
+					} else if (typeof chart[optionName] === 'function') {
+						chart[optionName](optionValue);
+					}
+				}
+			}
+		}
+
+		/**
    * Filter options base on predicates
    * @param {Array} keys          An array of keys to preserve or remove
    * @param {Function} predicate  The function used to filter keys
    */
-  options(keys, predicate) {
-	  // DEPRECATED: this.props.chartOptions
-    let opt = this.parsedProps.options || this.parsedProps || this.props.chartOptions;
-    predicate = predicate || pick;
-    return predicate(opt, keys);
-  }
 
-  /**
+	}, {
+		key: 'options',
+		value: function options(keys, predicate) {
+			// DEPRECATED: this.props.chartOptions
+			var opt = this.parsedProps.options || this.parsedProps || this.props.chartOptions;
+			predicate = predicate || _utils.pick;
+			return predicate(opt, keys);
+		}
+
+		/**
    * element resize callback function
    * @param  {Event} e
    */
-  resize(e) {
-        clearTimeout(this.resizeHandler);
-        this.resizeHandler = setTimeout(() => {
-                clearTimeout(this.resizeHandler);
-                if (this.chart && typeof this.chart.update === "function") this.chart.update();
-        }, 250);
-  }
 
-  /**
+	}, {
+		key: 'resize',
+		value: function resize(e) {
+			var _this3 = this;
+
+			clearTimeout(this.resizeHandler);
+			this.resizeHandler = setTimeout(function () {
+				clearTimeout(_this3.resizeHandler);
+				if (_this3.chart && typeof _this3.chart.update === "function") _this3.chart.update();
+			}, 250);
+		}
+
+		/**
    * Render function
    * svg element needs to have height and width.
    */
-  render() {
-    let size = pick(this.props, SIZE);
-    let style = Object.assign({}, size, this.props.containerStyle);
-    return (
-      <div ref="root" className="nv-chart" style={style} >
-        <svg ref="svg" {...size}></svg>
-      </div>
-    );
-  }
-}
+
+	}, {
+		key: 'render',
+		value: function render() {
+			var size = (0, _utils.pick)(this.props, SIZE);
+			var style = Object.assign({}, size, this.props.containerStyle);
+			return _react2.default.createElement(
+				'div',
+				{ ref: 'root', className: 'nv-chart', style: style },
+				_react2.default.createElement('svg', _extends({ ref: 'svg' }, size))
+			);
+		}
+	}]);
+
+	return NVD3Chart;
+}(_react2.default.Component);
+
+exports.default = NVD3Chart;
+
+
+NVD3Chart.propTypes = {
+	type: _react2.default.PropTypes.string.isRequired,
+	configure: _react2.default.PropTypes.func
+};
 
 // Babel 6 issue: http://stackoverflow.com/questions/33505992/babel-6-changes-how-it-exports-default
 module.exports = NVD3Chart;
